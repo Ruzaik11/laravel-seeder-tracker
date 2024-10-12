89cd9edc062049df0991b8e5263c4dcf7e570d41
Ruzaik <mnmruzaik@gmail.com>
2024-10-12 10:15:00 -0400

    Create basic package structure
    - Add service provider skeleton
    - Set up directory structure for commands, models, contracts
    - Prepare test directories

---
+++ src/SeederTrackerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ruzaik11\SeederTracker;
+
+use Illuminate\Support\ServiceProvider;
+
+class SeederTrackerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
