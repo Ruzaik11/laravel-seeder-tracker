faf7e2c9a0752695b4ecc3b7ba9f3e1c292fdfa4
Ruzaik <mnmruzaik@gmail.com>
2024-11-19 16:45:00 -0500

    implement TrackableSeeder base class
    - create abstact base class for trackable seeders
    - implement tracking logic with duplciate prevention
    - add automatic batch assignment
    - force implementation of seedData method in childs classes

---
+++ src/Seeders/TrackableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ruzaik11\SeederTracker\Seeders;
+
+use Illuminate\Database\Seeder;
+use Ruzaik11\SeederTracker\Contracts\TrackableSeederInterface;
+use Ruzaik11\SeederTracker\Models\SeederTracking;
+use Carbon\Carbon;
+
+abstract class TrackableSeeder extends Seeder implements TrackableSeederInterface
+{
+    public function hasBeenExecuted(): bool
+    {
+        return SeederTracking::hasBeenExecuted(get_class($this));
+    }
+
+    public function resetTracking(): void
+    {
+        SeederTracking::where('seeder_name', get_class($this))->delete();
+    }
+
+    abstract public function seedData();
+
+    public function run()
+    {
+        if ($this->hasBeenExecuted()) {
+            $this->command->info("Seeder " . get_class($this) . " already executed. Skipping...");
+            return;
+        }
+
+        $this->seedData();
+        
+        SeederTracking::create([
+            'seeder_name' => get_class($this),
+            'executed_at' => Carbon::now(),
+            'batch' => SeederTracking::getNextBatch(),
+        ]);
+    }
+}
