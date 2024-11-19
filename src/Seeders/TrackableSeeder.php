7614ececea65585ff1e174d65465566d9857893b
Ruzaik <mnmruzaik@gmail.com>
2024-11-19 09:30:00 -0500

    integrate PerformanceMonitor into TrackableSeeder
    - use performance monitoring trait in base class
    - automatically track execution metrics
    - store performance data in metadata
    - show execution time in console output

--- src/Seeders/TrackableSeeder.php
+++ src/Seeders/TrackableSeeder.php
@@ -5,10 +5,13 @@ namespace Ruzaik11\SeederTracker\Seeders;
 use Illuminate\Database\Seeder;
 use Ruzaik11\SeederTracker\Contracts\TrackableSeederInterface;
 use Ruzaik11\SeederTracker\Models\SeederTracking;
+use Ruzaik11\SeederTracker\Traits\PerformanceMonitor;
 use Carbon\Carbon;
 
 abstract class TrackableSeeder extends Seeder implements TrackableSeederInterface
 {
+    use PerformanceMonitor;
+
     public function hasBeenExecuted(): bool
     {
         return SeederTracking::hasBeenExecuted(get_class($this));
@@ -28,12 +31,19 @@ abstract class TrackableSeeder extends Seeder implements TrackableSeederInterfac
             return;
         }
 
+        $this->startPerformanceTracking();
+        
         $this->seedData();
+        
+        $performanceData = $this->endPerformanceTracking();
         
         SeederTracking::create([
             'seeder_name' => get_class($this),
             'executed_at' => Carbon::now(),
             'batch' => SeederTracking::getNextBatch(),
+            'metadata' => $performanceData,
         ]);
+
+        $this->command->info("Seeder executed in {$performanceData['execution_time_ms']}ms");
     }
 }
