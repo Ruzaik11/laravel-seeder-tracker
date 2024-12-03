3c912fe47f4dbf2600005a6f7fbd021287ddd045
Ruzaik <mnmruzaik@gmail.com>
2024-12-03 10:00:00 -0500

    Add configuration suport and enhance service provider
    - create configurable options for pakage behavior
    - add publishable config file
    - enable migration publishing
    - register artisan commands
    - auto-load pakage migrations

---
+++ config/seeder-tracker.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'table' => 'seeder_tracking',
+    'auto_track' => true,
+    'prevent_duplicates' => env('SEEDER_PREVENT_DUPLICATES', true),
+    'strict_environments' => ['production'],
+];
--- src/SeederTrackerServiceProvider.php
+++ src/SeederTrackerServiceProvider.php
@@ -3,16 +3,31 @@
 namespace Ruzaik11\SeederTracker;
 
 use Illuminate\Support\ServiceProvider;
+use Ruzaik11\SeederTracker\Commands\SeederStatusCommand;
 
 class SeederTrackerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__.'/../config/seeder-tracker.php', 'seeder-tracker');
     }
 
     public function boot()
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/seeder-tracker.php' => config_path('seeder-tracker.php'),
+            ], 'seeder-tracker-config');
+
+            $this->publishes([
+                __DIR__.'/database/migrations/' => database_path('migrations'),
+            ], 'seeder-tracker-migrations');
+
+            $this->commands([
+                SeederStatusCommand::class,
+            ]);
+        }
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 }
